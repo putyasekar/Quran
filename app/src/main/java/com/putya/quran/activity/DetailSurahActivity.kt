8a02fad7f6e61b3286f1e7513ff414c0fb51fd4a
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-10-13 11:13:06 +0700

    UI done

---
+++ app/src/main/java/com/putya/quran/activity/DetailSurahActivity.kt
@@ -0,0 +1,12 @@
+package com.putya.quran.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putya.quran.R
+
+class DetailSurahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_surah)
+    }
+}
---
+++ app/src/main/java/com/putya/quran/activity/MasjidActivity.kt
@@ -0,0 +1,12 @@
+package com.putya.quran.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putya.quran.R
+
+class MasjidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_masjid)
+    }
+}
